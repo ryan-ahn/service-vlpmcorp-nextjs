d3739227de07307949008b97fb3f50b79693d446
Ryan <kaaiinn4@gmail.com>
2022-08-23 03:26:30 +0900

    wip: 작업 중

--- containers/meta.ts
+++ containers/meta.ts
@@ -1,5 +1,5 @@
 export const META_COMMON = {
-  site_name: '테스트',
+  site_name: 'VLPM | 전국 가전 제품 견적 비교',
   site_description: '전국 가전 제품 견적 비교',
   site_keyword: '가전제품,가전,삼성,엘지,위니아,전자랜드,하이마트,견적비교,신혼부부',
   site_url: 'https://vlpm.io',
